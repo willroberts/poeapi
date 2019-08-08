a3c6afed56a95ba3bd61158746cdeca931fd3935
Will Roberts <will.roberts11@gmail.com>
2019-08-08 11:27:57 -0700

    Renames Rule->LeagueRule

--- types.go
+++ types.go
@@ -4,18 +4,18 @@ import "time"
 
 // League represents a permanent or challenge league.
 type League struct {
-	Name         string    `json:"id"`
-	Realm        string    `json:"realm"`
-	Description  string    `json:"description"`
-	LadderURL    string    `json:"url"`
-	StartTime    time.Time `json:"startAt"`
-	EndTime      time.Time `json:"endAt"`
-	DelveEnabled bool      `json:"delveEvent"`
-	Rules        []Rule    `json:"rules"`
+	Name         string       `json:"id"`
+	Realm        string       `json:"realm"`
+	Description  string       `json:"description"`
+	LadderURL    string       `json:"url"`
+	StartTime    time.Time    `json:"startAt"`
+	EndTime      time.Time    `json:"endAt"`
+	DelveEnabled bool         `json:"delveEvent"`
+	Rules        []LeagueRule `json:"rules"`
 }
 
-// Rule represents a modifier placed on a league.
-type Rule struct {
+// LeagueRule represents a modifier placed on a league.
+type LeagueRule struct {
 	ID          string `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
