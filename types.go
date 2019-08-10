70924d353f5cbb6f071518d6fda109889a8022d3
Will Roberts <will.roberts11@gmail.com>
2019-08-10 13:27:33 -0700

    Adds Item type

--- types.go
+++ types.go
@@ -100,13 +100,14 @@ type PVPMatch struct {
 	RegisterTime  time.Time `json:"registerAt"`
 }
 
-// StashResponse ...
+// StashResponse represents a response from the stash tab API which contains
+// multiples stashes.
 type StashResponse struct {
 	NextChangeID string  `json:"next_change_id"`
 	Stashes      []Stash `json:"stashes"`
 }
 
-// Stash ...
+// Stash represents a single public stash tab.
 type Stash struct {
 	ID                string `json:"id"`
 	AccountName       string `json:"accountName"`
@@ -117,5 +118,52 @@ type Stash struct {
 	Public            bool   `json:"public"`
 }
 
-// Item ...
-type Item struct{}
+// Item represents an item listing in the stash tab API.
+type Item struct {
+	Corrupted         bool          `json:"corrupted"`
+	ExplicitMods      []string      `json:"explicitMods"`
+	FlavorText        []string      `json:"flavourText"`
+	FrameType         int64         `json:"frameType"`
+	Height            int64         `json:"h"`
+	ID                string        `json:"id"`
+	Icon              string        `json:"icon"`
+	Identified        bool          `json:"identified"`
+	ImplicitMods      []string      `json:"implicitMods"`
+	InventoryID       string        `json:"inventoryId"`
+	ItemLevel         int64         `json:"ilvl"`
+	League            string        `json:"league"`
+	LockedToCharacter bool          `json:"lockedToCharacter"`
+	Name              string        `json:"name"`
+	Note              string        `json:"note"`
+	Properties        []Property    `json:"properties"`
+	Requirements      []Requirement `json:"requirements"`
+	SocketedItems     interface{}   `json:"socketedItems"`
+	Sockets           []Socket      `json:"sockets"`
+	Support           bool          `json:"support"`
+	TalismanTier      int64         `json:"talismanTier"`
+	TypeLine          string        `json:"typeLine"`
+	Verified          bool          `json:"verified"`
+	Width             int64         `json:"w"`
+	XPosition         int64         `json:"x"`
+	YPosition         int64         `json:"y"`
+}
+
+// Property represents a property of an Item.
+type Property struct {
+	DisplayMode int64           `json:"displayMode"`
+	Name        string          `json:"name"`
+	Values      [][]interface{} `json:"values"`
+}
+
+// Requirement represents an attribute requirement on an Item, such as strength.
+type Requirement struct {
+	DisplayMode int64           `json:"displayMode"`
+	Name        string          `json:"name"`
+	Values      [][]interface{} `json:"values"`
+}
+
+// Socket represents a socket on an item, including its color and links.
+type Socket struct {
+	Attribute string `json:"attr"`
+	Group     int64  `json:"group"`
+}
