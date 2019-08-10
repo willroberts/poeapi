6638e84db56516eb36e11cdedc644b2c59c54b29
Will Roberts <will.roberts11@gmail.com>
2019-08-10 00:07:58 -0700

    Updates comment

--- stash_tabs.go
+++ stash_tabs.go
@@ -1,5 +1,7 @@
 package poeapi
 
 const (
+	// In order to avoid downloading every stash ever published, skip ahead to
+	// the latest stash ID as seen by poe.ninja.
 	nextChangeURL = "https://poe.ninja/api/Data/GetStats"
 )
