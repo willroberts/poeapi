052f8b532ac49f48b8cb0a39a125c7f0c542ccb6
Will Roberts <will.roberts11@gmail.com>
2019-08-10 11:06:19 -0700

    Adds validation

--- pvp_matches.go
+++ pvp_matches.go
@@ -6,16 +6,25 @@ import (
 	"net/url"
 )
 
+const seasonMatchType = "season"
+
 // GetPVPMatchesOptions contains the request parameters for the PVP matches
 // endpoint. All parameters are optional.
 type GetPVPMatchesOptions struct {
-	Type   string
+	// The type of matches to find. Set to 'season' to search for a specific PVP
+	// season, or leave blank to return all upcoming PVP matches.
+	Type string
+
+	// The name of the season for which to retrieve matches.
 	Season string
-	Realm  string
+
+	// The realm of PVP matches to retrieve.
+	// Valid options: 'pc', 'xbox', or 'sony'.
+	Realm string
 }
 
-// ToQueryParams converts PVP matches options to a URL parameter string.
-func (opts GetPVPMatchesOptions) ToQueryParams() string {
+// toQueryParams converts PVP matches options to a URL parameter string.
+func (opts GetPVPMatchesOptions) toQueryParams() string {
 	u := url.Values{}
 	if opts.Type != "" {
 		u.Add("type", opts.Type)
@@ -29,10 +38,25 @@ func (opts GetPVPMatchesOptions) ToQueryParams() string {
 	return u.Encode()
 }
 
+func validateGetPVPMatchesOptions(opts GetPVPMatchesOptions) error {
+	if opts.Type == seasonMatchType && opts.Season == "" {
+		return ErrInvalidSeason
+	}
+	if opts.Realm != "" {
+		if _, ok := validRealms[opts.Realm]; !ok {
+			return ErrInvalidRealm
+		}
+	}
+	return nil
+}
+
 // GetPVPMatches retrieves all PVP matches from the API.
 func (c *client) GetPVPMatches(opts GetPVPMatchesOptions) ([]PVPMatch, error) {
+	if err := validateGetPVPMatchesOptions(opts); err != nil {
+		return []PVPMatch{}, err
+	}
 	url := fmt.Sprintf("%s?%s", c.formatURL(pvpMatchesEndpoint),
-		opts.ToQueryParams())
+		opts.toQueryParams())
 	resp, err := c.get(url)
 	if err != nil {
 		return []PVPMatch{}, err
