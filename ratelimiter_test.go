20fa0fd8c8c20ab26bb7a0409824c4e7d6723e4b
Will Roberts <will.roberts11@gmail.com>
2019-08-11 16:11:06 -0700

    Cleans up ratelimiter tests

--- ratelimiter_test.go
+++ ratelimiter_test.go
@@ -9,38 +9,15 @@ import (
 func TestRateLimiter(t *testing.T) {
 	var (
 		rateLimit    = 10.0
-		requestCount = 0
-		testDuration = 2.0 // Seconds.
-		r            = newRateLimiter(rateLimit, rateLimit)
-	)
-
-	timer := time.NewTimer(time.Duration(testDuration) * time.Second)
-	defer timer.Stop()
-
-	for i := 0; i < 20; i++ {
-		r.wait(false)
-		requestCount++
-	}
-	<-timer.C
-
-	if requestCount > int(rateLimit*testDuration) {
-		t.Fatalf("ratelimiter failed: saw %d requests in %.1f seconds (expected %d)",
-			requestCount, testDuration, int(rateLimit*testDuration))
-	}
-}
-
-func TestRateLimiterTooFast(t *testing.T) {
-	var (
-		rateLimit    = 1.0
 		requestCount uint32
-		testDuration = 5.0 // Seconds.
-		r            = newRateLimiter(rateLimit, rateLimit)
+		testDuration = 2.0 // Seconds.
+		r            = newRateLimiter(rateLimit, DefaultStashRateLimit)
 	)
 
 	timer := time.NewTimer(time.Duration(testDuration) * time.Second)
 	defer timer.Stop()
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 25; i++ {
 		go func() {
 			r.wait(false)
 			atomic.AddUint32(&requestCount, 1)
