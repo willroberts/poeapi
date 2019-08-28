26a195a09c0cb1106499bc96cd048fc1a0879a22
Will Roberts <will.roberts11@gmail.com>
2019-08-28 13:13:19 -0700

    Updates GoDoc

--- doc.go
+++ doc.go
@@ -9,13 +9,13 @@ Usage:
 	clientOpts := poeapi.ClientOptions{
 	    Host:              "api.pathofexile.com", // The primary API domain.
 	    NinjaHost:         "poe.ninja",           // Used to get latest stash ID.
-    	UseSSL:            true,                  // Use HTTPS for requests.
-    	UseCache:          true,                  // Enable the in-memory cache.
-    	UseDNSCache:       true,                  // Enable the in-memory DNS resolution cache.
-    	CacheSize:         200,                   // Number of items to store.
-    	RateLimit:         4.0,                   // Requests per second.
-    	StashRateLimit:    1.0,                   // Requests per second for trade API.
-    	RequestTimeout:    5 * time.Second        // Time to wait before canceling requests.
+	    UseSSL:            true,                  // Use HTTPS for requests.
+	    UseCache:          true,                  // Enable the in-memory cache.
+	    UseDNSCache:       true,                  // Enable the in-memory DNS resolution cache.
+	    CacheSize:         200,                   // Number of items to store.
+	    RateLimit:         4.0,                   // Requests per second.
+	    StashRateLimit:    1.0,                   // Requests per second for trade API.
+	    RequestTimeout:    5 * time.Second        // Time to wait before canceling requests.
 	} // This is equivalent to poeapi.DefaultClientOptions.
 
 	client, err := poeapi.NewAPIClient(clientOpts)
@@ -24,12 +24,12 @@ Usage:
 	}
 
 	ladder, err := client.GetLadder(poeapi.GetLadderOptions{
-    	ID:    "SSF Hardcore",
-    	Realm: "pc",
-    	Type:  "league",
+	    ID:    "SSF Hardcore",
+	    Realm: "pc",
+	    Type:  "league",
 	})
 	// Etc.
 
-See the `APIClient` interface for the full description of methods available.
+See the APIClient interface for the full description of methods available.
 */
 package poeapi
